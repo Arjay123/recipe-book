14d5e631f068b0dd32a9fbfe1743283bb4c1c867
arjaynguyen <arjaynguyen123@hotmail.com>
2017-09-04 21:31:17 -0700

    recipe base layout

--- app/src/index.js
+++ app/src/index.js
@@ -5,5 +5,39 @@ import registerServiceWorker from './registerServiceWorker';
 
 const hello = <h1>Hello, world!</h1>;
 
-ReactDOM.render(hello, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        var classes = "recipe";
+        return (
+            <div className={classes}>
+                    <Ingredients />
+                    <Directions />
+            </div>
+        );
+    }
+}
+
+class Ingredients extends React.Component {
+    render() {
+        var classes = "ingredients";
+        return (
+            <div className={classes}>
+                Ingredients
+            </div>
+        );
+    }
+}
+
+class Directions extends React.Component {
+    render() {
+        var classes = "directions";
+        return (
+            <div className={classes}>
+                Directions
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
